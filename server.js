4a3a03cca164fa84bae3a9e1e263b05b6a38bc09
CRM User <crm@vendas.com>
2026-05-19 11:12:27 -0300

    correcao

--- server.js
+++ server.js
@@ -155,10 +155,12 @@ app.get('/contato', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contato.html'));
 });
 
+const DB_PATH = path.join(__dirname, 'crm_database.sqlite');
+
 // Inicializa o banco SQLite (criará um arquivo crm_database.sqlite na mesma pasta)
-const db = new sqlite3.Database('./crm_database.sqlite', (err) => {
+const db = new sqlite3.Database(DB_PATH, (err) => {
     if (err) console.error('Erro ao abrir o banco de dados', err.message);
-    else console.log('Conectado ao banco SQLite com sucesso!');
+    else console.log(`Conectado ao banco SQLite com sucesso: ${DB_PATH}`);
 });
 
 function dbRunAsync(sql, params = []) {
